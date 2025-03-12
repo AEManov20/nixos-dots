db8c575ccc7748e99b64335f2eba48f21b5cf6c6
Alexander Manov <AEManov20@codingburgas.bg>
2025-03-13 00:26:33 +0200

    Update wyvern's hardware configuration

--- hardware/wyvern/default.nix
+++ hardware/wyvern/default.nix
@@ -1,4 +1,14 @@
 { ... }:
 {
+  hardware.graphics.enable = true;
 
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    powerManagement.finegrained = true;
+    open = true;
+    nvidiaSettings = true;
+  };
 }
