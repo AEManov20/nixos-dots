a4881e0c739979bcaa7bf60ab823117f81bb7f4c
Alexander Manov <AEManov20@codingburgas.bg>
2025-04-05 09:42:21 +0300

    falkor: Add adb

--- software/machine-specific/falkor/default.nix
+++ software/machine-specific/falkor/default.nix
@@ -1,12 +1,17 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [ ../../. ];
 
   networking.hostName = "falkor";
 
   boot.kernelParams = [ "i915.force_probe=7d55" ];
+
+  programs.adb.enable = true;
   
   services.throttled.enable = false;
+  services.udev.packages = [
+    pkgs.android-udev-rules
+  ];
 
   system.stateVersion = "24.11";
 }
--- software/users.nix
+++ software/users.nix
@@ -2,13 +2,12 @@
 {
   users.users.alex = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "libvirtd" "wireshark" "docker" "uucp" "dialout" "tty" ];
+    extraGroups = [ "wheel" "libvirtd" "wireshark" "docker" "uucp" "dialout" "tty" "adbusers" ];
     packages = with pkgs; [
       bitwarden-desktop
       (chromium.override { enableWideVine = true; })
       dbeaver-bin
       wireshark
-      keepassxc
       bottles
       libreoffice
       zed-editor
