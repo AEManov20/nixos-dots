bbc22062405a5873953e154d2fe3177e5a2f6e87
Alexander Manov <AEManov20@codingburgas.bg>
2025-05-31 12:12:44 +0300

    networking: update DNS config

--- software/networking.nix
+++ software/networking.nix
@@ -1,14 +1,5 @@
 { ... }:
 {
   networking.networkmanager.enable = true;
-
-  networking.nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
-
-  services.resolved = {
-    enable = true;
-    dnssec = "true";
-    domains = [ "~." ];
-    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
-    dnsovertls = "true";
-  };
+  services.resolved.enable = true;
 }
