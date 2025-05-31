a0016f4473f3e4cfdfdc6bb40a29ba2c1858efde
Alexander Manov <AEManov20@codingburgas.bg>
2025-05-31 12:12:17 +0300

    mount(pistorage): make falkor use tailscale host, make wyvern use local network ip

--- software/machine-specific/falkor/default.nix
+++ software/machine-specific/falkor/default.nix
@@ -14,6 +14,16 @@
     pkgs.android-udev-rules
   ];
 
+  fileSystems."/mnt/pistorage" = {
+    device = "//pi/pi_storage";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,nofail,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
+  };
+
   boot.kernelPackages = pkgs.linuxPackages;
 
   system.stateVersion = "24.11";
--- software/services.nix
+++ software/services.nix
@@ -14,14 +14,4 @@
   services.tumbler.enable = true; # Thumbnail support for images
   services.power-profiles-daemon.enable = true;
   services.fwupd.enable = true;
-
-  fileSystems."/mnt/pistorage" = {
-    device = "//pi/pi_storage";
-    fsType = "cifs";
-    options = let
-      # this line prevents hanging on network split
-      automount_opts = "x-systemd.automount,noauto,nofail,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-
-    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
-  };
 }
