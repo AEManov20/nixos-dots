cb057d4e904e4db7dafdcf6a2b0a567c172571d7
Alexander Manov <alexzanpy+github.aemanov20@proton.me>
2025-11-10 09:52:07 +0100

    software(fonts): noto-fonts-emoji (deprecated) -> noto-fonts-color-emoji

--- software/fonts.nix
+++ software/fonts.nix
@@ -4,7 +4,7 @@
     packages = with pkgs; [
       noto-fonts
       noto-fonts-cjk-sans
-      noto-fonts-emoji
+      noto-fonts-color-emoji
       liberation_ttf
       ibm-plex
       texlivePackages.fontawesome
