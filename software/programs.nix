fe3313d1424639ca658c558e593e80f3a181fecc
Alexander Manov <alexzanpy+github.aemanov20@proton.me>
2025-11-13 16:03:57 +0100

    software: -firefox, -cifs, -thunar, -vscode

--- software/programs.nix
+++ software/programs.nix
@@ -25,14 +25,6 @@
     openFirewall = true;
   };
 
-  programs.thunar = {
-    enable = true;
-    plugins = with pkgs.xfce; [
-      thunar-archive-plugin
-      thunar-volman
-    ];
-  };
-
   programs.starship = {
     enable = true;
     settings = {
@@ -62,51 +54,51 @@
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
 
-  programs.firefox = {
-    enable = true;
-    nativeMessagingHosts.packages = [ pkgs.tridactyl-native ];
-    policies = {
-      DisableTelemetry = true;
-      DisableFirefoxStudies = true;
-      Preferences = {
-        "cookiebanners.service.mode.privateBrowsing" = 2; # Block cookie banners in private browsing
-        "cookiebanners.service.mode" = 2; # Block cookie banners
-        "privacy.donottrackheader.enabled" = true;
-        "privacy.fingerprintingProtection" = true;
-        "privacy.resistFingerprinting" = true;
-        "privacy.trackingprotection.emailtracking.enabled" = true;
-        "privacy.trackingprotection.enabled" = true;
-        "privacy.trackingprotection.fingerprinting.enabled" = true;
-        "privacy.trackingprotection.socialtracking.enabled" = true;
-      };
-      ExtensionSettings = {
-        "tridactyl.vim@cmcaine.co.uk" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/file/4549492/tridactyl_vim-1.24.4.xpi";
-          installation_mode = "force_installed";
-	  updates_disabled = true;
-	  private_browsing = true;
-	};
-	"{73a6fe31-595d-460b-a920-fcc0f8843232}" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/file/4551629/noscript-13.0.9.xpi";
-	  installation_mode = "force_installed";
-	  updates_disabled = true;
-	  private_browsing = true;
-        };
-        "addon@darkreader.org" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/file/4535824/darkreader-4.9.110.xpi";
-	  installation_mode = "force_installed";
-	  updates_disabled = true;
-	  private_browsing = false;
-        };
-        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-	  install_url = "https://addons.mozilla.org/firefox/downloads/file/4562769/bitwarden_password_manager-2025.8.1.xpi";
-	  installation_mode = "force_installed";
-	  updates_disabled = true;
-	  private_browsing = true;
-        };
-      };
-    };
-  };
+  # programs.firefox = {
+  #   enable = true;
+  #   nativeMessagingHosts.packages = [ pkgs.tridactyl-native ];
+  #   policies = {
+  #     DisableTelemetry = true;
+  #     DisableFirefoxStudies = true;
+  #     Preferences = {
+  #       "cookiebanners.service.mode.privateBrowsing" = 2; # Block cookie banners in private browsing
+  #       "cookiebanners.service.mode" = 2; # Block cookie banners
+  #       "privacy.donottrackheader.enabled" = true;
+  #       "privacy.fingerprintingProtection" = true;
+  #       "privacy.resistFingerprinting" = true;
+  #       "privacy.trackingprotection.emailtracking.enabled" = true;
+  #       "privacy.trackingprotection.enabled" = true;
+  #       "privacy.trackingprotection.fingerprinting.enabled" = true;
+  #       "privacy.trackingprotection.socialtracking.enabled" = true;
+  #     };
+  #     ExtensionSettings = {
+  #       "tridactyl.vim@cmcaine.co.uk" = {
+  #         install_url = "https://addons.mozilla.org/firefox/downloads/file/4549492/tridactyl_vim-1.24.4.xpi";
+  #         installation_mode = "force_installed";
+  #         updates_disabled = true;
+  #         private_browsing = true;
+  #       };
+  #       "{73a6fe31-595d-460b-a920-fcc0f8843232}" = {
+  #         install_url = "https://addons.mozilla.org/firefox/downloads/file/4551629/noscript-13.0.9.xpi";
+  #         installation_mode = "force_installed";
+  #         updates_disabled = true;
+  #         private_browsing = true;
+  #       };
+  #       "addon@darkreader.org" = {
+  #         install_url = "https://addons.mozilla.org/firefox/downloads/file/4535824/darkreader-4.9.110.xpi";
+  #         installation_mode = "force_installed";
+  #         updates_disabled = true;
+  #         private_browsing = false;
+  #       };
+  #       "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+  #         install_url = "https://addons.mozilla.org/firefox/downloads/file/4562769/bitwarden_password_manager-2025.8.1.xpi";
+  #         installation_mode = "force_installed";
+  #         updates_disabled = true;
+  #         private_browsing = true;
+  #       };
+  #     };
+  #   };
+  # };
 
   programs.gnupg.agent = {
     enable = true;
@@ -121,7 +113,6 @@
     wayland-utils
     vlc
     mpv
-    cifs-utils
     rclone
     gitFull
     libreoffice
@@ -167,26 +158,6 @@
 
     opensc
     libp11
-
-    (vscode-with-extensions.override {
-      vscodeExtensions = with vscode-extensions; [
-        vscodevim.vim
-        ms-vscode.cmake-tools
-        vadimcn.vscode-lldb
-        jnoortheen.nix-ide
-        arrterian.nix-env-selector
-        mkhl.direnv
-	rust-lang.rust-analyzer
-	llvm-vs-code-extensions.vscode-clangd
-      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        {
-	  name = "vscode-liquidsoap";
-	  publisher = "savonet";
-	  version = "0.2.0";
-	  hash = "sha256-CClN1MU/qyGw969Zp7SCNDOvJM4qsWHaHyag9oNdqGQ=";
-	}
-      ];
-    })
   ];
 
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
