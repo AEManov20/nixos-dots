2c7e98216fa4b92e9ee933dcf1bd641d943d7480
Aleksandar Manov <alexzanpy+github.aemanov20@proton.me>
2025-11-20 11:33:54 +0100

    software(machine-specific(falkor)): add backup user and configure ssh for it

--- software/machine-specific/falkor/default.nix
+++ software/machine-specific/falkor/default.nix
@@ -4,22 +4,25 @@
 
   networking.hostName = "falkor";
 
+  users.users.backup = {
+    isNormalUser = true;
+    shell = pkgs.bashInteractive;
+  };
+
   boot.kernelParams = [ "i915.force_probe=7d55" "acpi_backlight=native" ];
 
   programs.adb.enable = true;
   
-  # services.blueman.enable = true;
   services.throttled.enable = false;
-  services.udev.packages = [
-    pkgs.android-udev-rules
-  ];
+
+  services.openssh.settings.AllowUsers = [ "backup" ];
 
   fileSystems."/mnt/pistorage" = {
     device = "alex@pi:/mnt/pi_storage";
     fsType = "sshfs";
     options = let
       # this line prevents hanging on network split
-      automount_opts = "x-systemd.automount,noauto,nofail,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
 
     in ["${automount_opts},nodev,noatime,allow_other,IdentityFile=/root/.ssh/id_ed25519_falkor,gid=100"];
   };
