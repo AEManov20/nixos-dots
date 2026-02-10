e105b0ace48aa62015bc4c8b07628618fc865d22
Aleksandar Manov <alexzanpy+github.aemanov20@proton.me>
2026-02-10 21:53:37 +0100

    bootloader: add plymouth

--- software/bootloader.nix
+++ software/bootloader.nix
@@ -3,5 +3,21 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.edk2-uefi-shell.enable = true;
+  boot.loader.timeout = 0;
+
+  boot.consoleLogLevel = 3;
+  boot.initrd.verbose = false;
+  boot.kernelParams = [
+    "quiet"
+    "udev.log_level=3"
+    "systemd.show_status=auto"
+  ];
+
   boot.supportedFilesystems = [ "nfs" ];
+
+  boot.plymouth = {
+    enable = true;
+  };
+
+  boot.initrd.systemd.enable = true;
 }
