ecf786171402f5755c3b4b1ea17a9d642f9c621e
Alexander Manov <alexzanpy+github.aemanov20@proton.me>
2026-02-15 23:45:30 +0100

    software(desktop(pipewire)): implement noise suppression

--- software/desktop.nix
+++ software/desktop.nix
@@ -2,10 +2,50 @@
 {
   services.libinput.enable = true;
 
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     pulse.enable = true;
     jack.enable = true;
+
+    extraLv2Packages = [ pkgs.rnnoise-plugin ];
+    configPackages = [
+      (pkgs.writeTextDir "share/pipewire/pipewire.conf.d/99-input-denoising.conf" ''
+        context.modules = [
+        {   name = libpipewire-module-filter-chain
+            args = {
+                node.description =  "Noise Canceling source"
+                media.name =  "Noise Canceling source"
+                filter.graph = {
+                    nodes = [
+                        {
+                            type = ladspa
+                            name = rnnoise
+                            plugin = /nix/store/7n1fbs83hiykr17yms5dsjhbvrvam7kp-rnnoise-plugin-1.10/lib/ladspa/librnnoise_ladspa.so
+                            label = noise_suppressor_mono
+                            control = {
+                                "VAD Threshold (%)" = 50.0
+                                "VAD Grace Period (ms)" = 200
+                                "Retroactive VAD Grace (ms)" = 0
+                            }
+                        }
+                    ]
+                }
+                capture.props = {
+                    node.name =  "capture.rnnoise_source"
+                    node.passive = true
+                    audio.rate = 48000
+                }
+                playback.props = {
+                    node.name =  "rnnoise_source"
+                    media.class = Audio/Source
+                    audio.rate = 48000
+                }
+            }
+        }
+        ]
+      '')
+    ];
   };
 
   services.displayManager.sddm = {
