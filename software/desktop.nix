9b3206a3ee57c01e184feba4123b353f8bc34e1c
Aleksandar Manov <alexzanpy+github.aemanov20@proton.me>
2026-05-04 10:28:38 +0200

    software(desktop): remove rnnoise2

--- software/desktop.nix
+++ software/desktop.nix
@@ -8,44 +8,44 @@
     pulse.enable = true;
     jack.enable = true;
 
-    extraLv2Packages = [ pkgs.rnnoise-plugin ];
-    configPackages = [
-      (pkgs.writeTextDir "share/pipewire/pipewire.conf.d/99-input-denoising.conf" ''
-        context.modules = [
-        {   name = libpipewire-module-filter-chain
-            args = {
-                node.description =  "Noise Canceling source"
-                media.name =  "Noise Canceling source"
-                filter.graph = {
-                    nodes = [
-                        {
-                            type = ladspa
-                            name = rnnoise
-                            plugin = ${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so
-                            label = noise_suppressor_mono
-                            control = {
-                                "VAD Threshold (%)" = 90.0
-                                "VAD Grace Period (ms)" = 200
-                                "Retroactive VAD Grace (ms)" = 0
-                            }
-                        }
-                    ]
-                }
-                capture.props = {
-                    node.name =  "capture.rnnoise_source"
-                    node.passive = true
-                    audio.rate = 48000
-                }
-                playback.props = {
-                    node.name =  "rnnoise_source"
-                    media.class = Audio/Source
-                    audio.rate = 48000
-                }
-            }
-        }
-        ]
-      '')
-    ];
+    # extraLv2Packages = [ pkgs.rnnoise-plugin ];
+    # configPackages = [
+    #   (pkgs.writeTextDir "share/pipewire/pipewire.conf.d/99-input-denoising.conf" ''
+    #     context.modules = [
+    #     {   name = libpipewire-module-filter-chain
+    #         args = {
+    #             node.description =  "Noise Canceling source"
+    #             media.name =  "Noise Canceling source"
+    #             filter.graph = {
+    #                 nodes = [
+    #                     {
+    #                         type = ladspa
+    #                         name = rnnoise
+    #                         plugin = ${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so
+    #                         label = noise_suppressor_mono
+    #                         control = {
+    #                             "VAD Threshold (%)" = 90.0
+    #                             "VAD Grace Period (ms)" = 200
+    #                             "Retroactive VAD Grace (ms)" = 0
+    #                         }
+    #                     }
+    #                 ]
+    #             }
+    #             capture.props = {
+    #                 node.name =  "capture.rnnoise_source"
+    #                 node.passive = true
+    #                 audio.rate = 48000
+    #             }
+    #             playback.props = {
+    #                 node.name =  "rnnoise_source"
+    #                 media.class = Audio/Source
+    #                 audio.rate = 48000
+    #             }
+    #         }
+    #     }
+    #     ]
+    #   '')
+    # ];
   };
 
   services.displayManager.sddm = {
